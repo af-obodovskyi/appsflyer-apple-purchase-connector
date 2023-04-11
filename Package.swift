4e8978828cc7a6be2fbdda53c573d64857323691
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-04-11 23:09:18 +0300

    PAckage play around with targets

--- Package.swift
+++ Package.swift
@@ -10,7 +10,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PurchaseConnector",
-            targets: ["PurchaseConnector", "PurchaseConnector-Dynamic"])
+            targets: ["PurchaseConnector"]),
+        .library(
+            name: "PurchaseConnector-Dynamic"
+            targets: ["PurchaseConnector-Dynamic"])
     ],
     dependencies: [
         .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.10.0")
@@ -20,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "PurchaseConnector",
-            path: "PurchaseConnector.xcframework"
+            path: "PurchaseConnector.xcframework",
         ),
         .binaryTarget(
             name: "PurchaseConnector-Dynamic",
