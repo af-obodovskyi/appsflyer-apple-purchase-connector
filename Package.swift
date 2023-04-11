47617db368f6460648234b55ffef2bb8cc016c90
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-04-11 15:10:42 +0300

    dynamic 2

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PurchaseConnector",
-            targets: ["PurchaseConnector"])
+            targets: ["PurchaseConnector", "PurchaseConnector-Dynamic"])
     ],
     dependencies: [
         .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.10.0")
