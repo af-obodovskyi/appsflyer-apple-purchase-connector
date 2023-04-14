b996228a05c6d3d16a74ae3790fce77d5bd161d0
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-04-14 13:40:27 +0300

    asdf

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["PurchaseConnector-Dynamic"])
     ],
     dependencies: [
-        .package(path: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git")
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.10.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
